bb41d7c0d033ff4417e3d024f4e17f46d802768a
Sam <samuel.obrien25@gmail.com>
2021-01-17 19:11:04 -0500

    fix linter issues

--- src/components/Buttons/LogInOutButton.vue
+++ src/components/Buttons/LogInOutButton.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="LogInOut__Wrapper">
-    <a 
-      class="ActionButton ActionButton--Constructive" 
-      v-if="inOut === 'in'" 
+    <a
+      class="ActionButton ActionButton--Constructive"
+      v-if="inOut === 'in'"
       href='/login'
     >
       Log In
     </a>
-    <a 
-      class="ActionButton ActionButton--Destructive" 
-      v-else 
+    <a
+      class="ActionButton ActionButton--Destructive"
+      v-else
       href='/logout'
     >
       Log Out
