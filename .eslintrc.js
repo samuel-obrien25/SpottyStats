7bfc64bead04e6f0910cd25e166937716d43c8d2
samuel-obrien25 <samuel.obrien25@gmail.com>
2021-11-17 15:55:51 -0500

    Upgrade entire project

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,17 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'linebreak-style': 'off',
+    'linebreak-style': ["off"],
   },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
